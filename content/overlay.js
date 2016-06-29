1b11006e3f7ca0ac369bb8791cdde50afbd026eb
babobski <info@babobski.nl>
2016-06-29 22:59:23 +0200

    updated version numbers for release

--- content/overlay.js
+++ content/overlay.js
@@ -3,7 +3,7 @@
  */
 if (typeof(extensions) === 'undefined') extensions = {};
 if (typeof(extensions.relativeIncludes) === 'undefined') extensions.relativeIncludes = {
-	version: '1.2'
+	version: '2.1'
 };
 
 (function() {
